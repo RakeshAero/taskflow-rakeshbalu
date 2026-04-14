b4105c7f22542eb4120086cbfda52dbd0126a5bb
RakeshAero <rakeshaero55743@gmail.com>
2026-04-14 06:49:41 -0700

    required modules

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,6 +1,8 @@
 package main
+
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log/slog"
 	"net/http"
@@ -8,14 +10,28 @@ import (
 	"os/signal"
 	"syscall"
 	"time"
- 
+
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/joho/godotenv"
- 
-	"github.com/RakeshAero/taskflow-rakeshbalu/internal/config"
-	"github.com/RakeshAero/taskflow-rakeshbalu/internal/db"
-	"github.com/RakeshAero/taskflow-rakeshbalu/internal/handlers"
-	authmw "github.com/RakeshAero/taskflow-rakeshbalu/internal/middleware"
-	"github.com/RakeshAero/taskflow-rakeshbalu/internal/repository"
+
+	"github.com/RakeshAero/taskflow-rakeshbalu/backend/internal/config"
+	"github.com/RakeshAero/taskflow-rakeshbalu/backend/internal/db"
+	"github.com/RakeshAero/taskflow-rakeshbalu/backend/internal/handlers"
+	authmw "github.com/RakeshAero/taskflow-rakeshbalu/backend/internal/middleware"
+	"github.com/RakeshAero/taskflow-rakeshbalu/backend/internal/repository"
 )
+
+func main(){
+	route := chi.NewRouter()
+	route.Get("/ping", func(w http.ResponseWriter, r *http.Request){
+		w.Header().Set("Content-Type","application/json")
+		w.WriteHeader(http.StatusOK)
+
+		json.NewEncoder(w).Encode(map[string]string{
+			"message" : "pong",
+		})
+	})
+
+	http.ListenAndServe(":3000", route)
+}
